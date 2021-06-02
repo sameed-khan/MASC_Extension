a6fb9ffc4a4472162662c32655abae5a170028b2
Saadyg <49032723+Saadyg@users.noreply.github.com>
2021-06-02 10:52:48 -0400

    working test

--- background.js
+++ background.js
@@ -2,27 +2,5 @@
 let parser = new DOMParser()
 
 chrome.runtime.onMessage.addListener(async function(request, sender, sendReply) {
-    let ISBNs = request.msg
-    console.log(ISBNs)
-
-    ISBNs.forEach(async element => {
-        let x = await fetchLibGen(element)
-        console.log(x)
-    });
+    
 })
-
-
-const fetchLibGen = async (ISBN) => {
-    let request = new Promise((resolve, reject) => {
-                    fetch(`https://libgen.lc/search.php?req=${ISBN}&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def`)
-                        .then(res => res.text())
-                        .then(html => {
-                            let doc = parser.parseFromString(html, "text/html")
-                            resolve(doc)
-                        })
-                })
-
-    let data = await request
-
-    return data
-}
---
+++ testreplace.js
@@ -0,0 +1,3 @@
+const title = document.querySelector("h1.meta-article-title")
+
+title.innerHTML = "hello we replaced this text"
